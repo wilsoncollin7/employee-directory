b46f35d11aa3d0e090a1ececf87a6dc9cc6a9a25
wilsoncollin7 <wilsoncollin7@gmail.com>
2020-11-04 12:44:28 -0500

    added input and smaple table for styling

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,12 +1,21 @@
 import React from "react";
 // style
 import "./style.css";
+// import component
+import Table from "../Table/Table";
 
 function Card() {
   return (
     <div className="card">
       <div className="card-body">
-        <h3>this is the card</h3>
+        <h3>Search by name:</h3>
+        <input 
+          name="name"
+          type="text" 
+          className="form-control" 
+          placeholder="Name"
+        />
+        <Table />
       </div>
     </div>
   );
