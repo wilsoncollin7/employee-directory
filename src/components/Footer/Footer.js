eddbb7b6e79c5dd76e786ef352121234a10ffee4
wilsoncollin7 <wilsoncollin7@gmail.com>
2020-11-04 11:48:27 -0500

    added footer with social icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,28 @@
 import React from 'react';
+// style
+import "./style.css";
 
 function Footer () {
   return (
-    <h2>this is Footer</h2>
+    <footer className="footer">
+      <span>
+          <a class="btn btn-social-icon btn-github" id="footerIcon" href="https://github.com/wilsoncollin7" target="_">
+              <span class="fa fa-github"></span>
+          </a>
+          <a class="btn btn-social-icon btn-facebook" id="footerIcon" href="https://www.facebook.com/collin.wilson.167/" target="_">
+              <span class="fa fa-facebook"></span>
+          </a>
+          <a class="btn btn-social-icon btn-instagram" id="footerIcon" href="https://www.instagram.com/callingmewilson/" target="_">
+              <span class="fa fa-instagram"></span>
+          </a>
+          <a class="btn btn-social-icon btn-linkedin" id="footerIcon" href="https://www.linkedin.com/in/collin-wilson-a512351a8/" target="_">
+              <span class="fa fa-linkedin"></span>
+          </a>
+          <a class="btn btn-social-icon btn-youtube" id="footerIcon" href="https://www.youtube.com/channel/UCxEPolHlcRK6zDetwazkm9w?view_as=subscriber" target="_">
+              <span class="fa fa-youtube"></span>
+          </a>
+      </span>
+    </footer>
   );
 }
 
