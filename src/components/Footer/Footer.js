4c09eeff2da654698f804679158f9077611099f1
wilsoncollin7 <wilsoncollin7@gmail.com>
2020-11-04 12:22:43 -0500

    fixed row width problem and card centered

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,20 +6,20 @@ function Footer () {
   return (
     <footer className="footer">
       <span>
-          <a class="btn btn-social-icon btn-github" id="footerIcon" href="https://github.com/wilsoncollin7" target="_">
-              <span class="fa fa-github"></span>
+          <a className="btn btn-social-icon btn-github" id="footerIcon" href="https://github.com/wilsoncollin7" target="_">
+              <span className="fa fa-github"></span>
           </a>
-          <a class="btn btn-social-icon btn-facebook" id="footerIcon" href="https://www.facebook.com/collin.wilson.167/" target="_">
-              <span class="fa fa-facebook"></span>
+          <a className="btn btn-social-icon btn-facebook" id="footerIcon" href="https://www.facebook.com/collin.wilson.167/" target="_">
+              <span className="fa fa-facebook"></span>
           </a>
-          <a class="btn btn-social-icon btn-instagram" id="footerIcon" href="https://www.instagram.com/callingmewilson/" target="_">
-              <span class="fa fa-instagram"></span>
+          <a className="btn btn-social-icon btn-instagram" id="footerIcon" href="https://www.instagram.com/callingmewilson/" target="_">
+              <span className="fa fa-instagram"></span>
           </a>
-          <a class="btn btn-social-icon btn-linkedin" id="footerIcon" href="https://www.linkedin.com/in/collin-wilson-a512351a8/" target="_">
-              <span class="fa fa-linkedin"></span>
+          <a className="btn btn-social-icon btn-linkedin" id="footerIcon" href="https://www.linkedin.com/in/collin-wilson-a512351a8/" target="_">
+              <span className="fa fa-linkedin"></span>
           </a>
-          <a class="btn btn-social-icon btn-youtube" id="footerIcon" href="https://www.youtube.com/channel/UCxEPolHlcRK6zDetwazkm9w?view_as=subscriber" target="_">
-              <span class="fa fa-youtube"></span>
+          <a className="btn btn-social-icon btn-youtube" id="footerIcon" href="https://www.youtube.com/channel/UCxEPolHlcRK6zDetwazkm9w?view_as=subscriber" target="_">
+              <span className="fa fa-youtube"></span>
           </a>
       </span>
     </footer>
